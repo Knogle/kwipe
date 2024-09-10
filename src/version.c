018d84d30d2258a7da5d63a5b121de6b7318f1a7
Fabian Druschke <fdruschke@outlook.com>
2024-09-10 21:29:54 +0200

    Using repo commit hash instead of version number

--- src/version.c
+++ src/version.c
@@ -1,17 +1,16 @@
-/**
- * version_string and program_name are used by siege
- * and configure; author_name and email_address are
- * used by configure to dynamically assign those values
- * to documentation files.
- */
-const char* version_string = "0.37";
+#ifndef VERSION_HASH
+#define VERSION_HASH "unknown"
+#endif
+
+const char* version_string = VERSION_HASH;
 const char* program_name = "kwipe";
-const char* author_name = "Martijn van Brummelen";
-const char* email_address = "git@brumit.nl";
+const char* author_name = "Fabian Druschke";
+const char* email_address = "fabian@druschke.network";
 const char* years = "2024";
 const char* copyright = "Copyright Darik Horn <dajhorn-dban@vanadac.com>\n\
 Modifications to original dwipe Copyright Andy Beverley <andy@andybev.com>\n\
 This is free software; see the source for copying conditions.\n\
 There is NO warranty; not even for MERCHANTABILITY or FITNESS\n\
 FOR A PARTICULAR PURPOSE.\n";
-const char* banner = "kwipe 0.37";
+const char* banner = "kwipe " VERSION_HASH;
+
